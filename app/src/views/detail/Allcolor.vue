51c1db8205b4bfe37e1a47a27bc4ada5d6a1a603
周国平 <zhouguoping@zhouguopingdeMacBook-Pro.local>
2019-04-19 23:22:58 +0800

    详情页面组件

---
+++ app/src/views/detail/Allcolor.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="allcolor" @click="changeColor(zindex)" >
+        <div class="colorpic" :style="'background:'+zz.color"></div>
+        <span class="colorname">#{{zz.colorname}}</span>
+        <span class="iconfont icon-xiala"></span>
+    </div>
+</template>
+<script>
+export default {
+    name:'allcolor',
+    props:["zz","zindex"],
+    methods:{
+        //点击时通过触发父组件的方法，修改选中的颜色
+        changeColor:function(index){
+            // this.$emit('changechoosed',index)
+            this.$emit('changeChoosed',index);
+            console.log(index);
+        }
+    }
+}
+</script>
+<style scoped>
+   .allcolor{
+       width: 100%;
+       height: 44px;
+       box-sizing: border-box;
+       padding: 12px;
+   }
+   .allcolor:hover{
+       background: #eee;
+   }
+   .allcolor .colorpic{
+       width: 20px;
+       height: 20px;
+       background: rgb(65, 52, 52);
+       float: left;
+       margin-right: 40px;
+   }
+   .allcolor span{
+       line-height: 20px;
+       color: #999;
+   }
+</style>
+
+
---
+++ app/src/views/detail/Count.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="count" @click="changeCount(cindex)" >
+        <span class="countnum">数量:{{cindex}}</span>
+    </div>
+</template>
+<script>
+export default {
+    name:'count',
+    props:["cindex"],
+    methods:{
+        //点击时通过触发父组件的方法，修改选中的颜色
+        changeCount:function(index){
+            this.$emit('changeCount',index);
+        }
+    }
+}
+</script>
+<style scoped>
+   .count{
+       width: 100%;
+       height: 44px;
+       box-sizing: border-box;
+       padding: 12px;
+
+   }
+   .count:hover{
+       background: #eee;
+   }
+   .count span{
+       line-height: 20px;
+       color: #999;
+   }
+</style>
+
+
